a4137c796696809ae293e73cf90fb77245638e23
bivin <bivinvinod333@gmail.com>
2018-04-11 16:01:32 +0530

    added some designs

--- rentz/customer/booking_page.php
+++ rentz/customer/booking_page.php
@@ -23,31 +23,32 @@ $car=get_single_cars($carid);
 
 
 
-
+<div class="col-md-10 col-md-offset-1">
 
 <form id="frmaddcar" action="db_booking.php" method="post" enctype="multipart/form-data">
 	<div class="form-group">
         <label>Car Name</label>
-        <input type="text" class="form-control" id="carname" name="carname" value="<?php echo $car['car_company_name']; ?>">
+        <input type="text"  readonly class="form-control" id="carname" name="carname" value="<?php echo $car['car_company_name']; ?>">
  	 </div>
      <div class="form-group">
         <label>Car Colour</label>
-        <input type="text" class="form-control" id="carcolour" name="carcolour" value="<?php echo $car['car_colour']; ?>">
+        <input type="text" readonly  class="form-control" id="carcolour" name="carcolour" value="<?php echo $car['car_colour']; ?>">
  	 </div>
      <div class="form-group">
-        <label>Car Image</label>
-        <img src="../<?php echo $car['car_image']; ?>" width="300px" height="500px">
+        <label>Car Image</label><br/>
+        <img src="../<?php echo $car['car_image']; ?>" class="img-circle" width="300px" height="300px">
  	 </div>
      <div class="form-group">
         <label>Car Provider Details</label>
-        <input type="text" class="form-control" id="carprovider" name="carname" value="<?php echo $car['car_pro_id']; ?>">
+        <input type="text" readonly  class="form-control" id="carprovider" name="carname" value="<?php echo $car['car_pro_id']; ?>">
         <input type="hidden" class="form-control" id="carid" name="carid" value="<?php echo $carid; ?>">
          <input type="hidden" class="form-control" id="pid" name="pid" value="<?php echo $pid; ?>">
- 	 
+ 	 </div>
      <div class="form-group ">
        
-       	<button  type="submit" class="btn btn-primary">Submit</button>
+       	<button  type="submit" class="btn btn-primary">Book Now</button>
   		<button   type="cancel" class="btn btn-default">Cancel</button>
        
       </div> 
 </form>
+</div>
